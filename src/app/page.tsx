d028b58b9bf869cd727e2f4a2c06ef53e88e6a5f
Rachhen <chhenqq@gmail.com>
2025-06-15 10:03:40 +0700

    feat: enhance UI components with new styles and additional elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex flex-col gap-4 m-4">
+      <div>
+        <Button variant="reverse">Click me</Button>
+      </div>
+      <div>
+        <Input placeholder="Input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="Textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
