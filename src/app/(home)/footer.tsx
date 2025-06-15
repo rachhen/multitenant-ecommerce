6b853a62779dbcd9d8ff520ddbb340791896f2a5
Rachhen <chhenqq@gmail.com>
2025-06-15 19:12:21 +0700

    feat: add Footer and NavbarSidebar components, integrate them into Layout and Navbar

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6 bg-white">
+      <div className="flex items-center gap-2">
+        <p>funroad, Inc.</p>
+      </div>
+    </footer>
+  );
+}
